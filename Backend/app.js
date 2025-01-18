99c1cfcf9eb7455c585a08c23d0a42edb0f6f1a8
codersunny812 <sengersunny448@gmail.com>
2025-01-18 06:22:25 +0530

    file updated

--- Backend/app.js
+++ Backend/app.js
@@ -3,11 +3,16 @@ dotenv.config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const connectToDb = require('./DB/db')
+const connectToDb = require('./DB/db') //default import
+const userRouter = require('./Routes/user.routes')
 
 connectToDb() //function to connect with DB
 
-app.use(cors())   //use the cors 
+app.use(cors())//use the cors 
+app.use(express.json()) //to parse json request
+app.use(express.urlencoded({extended:true}))
+
+app.use('/users',userRouter)
 
 app.get('/',(req,res)=>{
     res.send("hello from the server")
