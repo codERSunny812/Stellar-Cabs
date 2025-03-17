708e6c250e000ecc9ee0354349d1bf0c399cbd1a
codersunny812 <sengersunny448@gmail.com>
2025-03-17 16:46:42 +0530

    login and signup page created

---
+++ Frontend/src/pages/caption.signup.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CaptionSignUp = () => {
+  return (
+    <div>CaptionSignUp</div>
+  )
+}
+
+export default CaptionSignUp;
---
+++ Frontend/src/pages/user.login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserLogin = () => {
+  return (
+    <div> user login component</div>
+  )
+}
+
+export default UserLogin;
